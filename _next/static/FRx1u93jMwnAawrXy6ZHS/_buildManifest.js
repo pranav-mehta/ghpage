57326b51532861e6fae4e97ef0af439cd787b923
pranav-mehta <34597580+pranav-mehta@users.noreply.github.com>
2022-11-25 21:21:08 +0000

    Deploying to gh-pages from @ pranav-mehta/ghpage@844e7edd070a42000a555fbaafefd8e42630641e 🚀

---
+++ _next/static/FRx1u93jMwnAawrXy6ZHS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/591-a8a810f1b325b551.js","static/chunks/pages/index-3535ac4e48f5cb7e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
